2a224ca3dd7c72445739b18f3380c44193485ba1
nichesoft34 <rahuldinesh1991@gmail.com>
2019-07-26 21:53:07 +0530

    [i18n] Preserve the selected language on reload/refresh

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -4,11 +4,11 @@ import i18n from '../i18n';
 export default () => (
   <select
     className="custom-select"
-    defaultValue="en"
+    defaultValue={i18n.language}
     onChange={e => i18n.changeLanguage(e.target.value)}
   >
     <option value="en">English</option>
     <option value="fr">French</option>
-    <option value="pl">French</option>
+    <option value="pl">Polish</option>
   </select>
 );
